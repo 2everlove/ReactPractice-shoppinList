e0d0e346c558f37b7afcefc6e69ddf12062fd8b7
mika <2everlove@gmail.com>
2023-11-23 03:11:06 +0900

    Oulet, Router Setting for IndexPage

--- mall/src/router/root.js
+++ mall/src/router/root.js
@@ -1,10 +1,12 @@
 import { Suspense, lazy } from 'react';
 const { createBrowserRouter} = require('react-router-dom');
 
-const Loading = <div>Loading....</div>
-const Main = lazy(() => import("../pages/MainPage"))
+const Loading = <div>Loading....</div>;
+const Main = lazy(() => import("../pages/MainPage"));
 
-const About = lazy(() => import("../pages/AboutPage"))
+const About = lazy(() => import("../pages/AboutPage"));
+
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
 
 const root = createBrowserRouter([
     {
@@ -14,6 +16,10 @@ const root = createBrowserRouter([
     {
         path: "about",
         element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+    {
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>
     }
 ]);
 
