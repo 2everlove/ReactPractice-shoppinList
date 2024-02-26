79ec1c816b8f1f3260d4bfba6942cd5938d881a2
mika <2everlove@gmail.com>
2024-02-26 22:25:30 +0900

    set React-Router

--- mall/src/router/root.js
+++ mall/src/router/root.js
@@ -1,5 +1,6 @@
 import { Suspense, lazy } from 'react';
 import todoRouter from './todoRouter';
+import productsRouter from './productsRouter';
 
 const { createBrowserRouter} = require('react-router-dom');
 
@@ -9,6 +10,8 @@ const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
 
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
+
+const ProductsIndex = lazy(() => import("../pages/products/IndesPage"));
 
 const root = createBrowserRouter([
     {
@@ -23,6 +26,11 @@ const root = createBrowserRouter([
         path: "todo",
         element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
         children:todoRouter()
+    },
+    {
+        path: "products",
+        element: <Suspense fallback={Loading}><ProductsIndex/></Suspense>,
+        children:productsRouter()
     }
     
 ]);
